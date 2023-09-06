aa7196dd7869ee80c8e9980e278382ac6fd51a1c
Vito di venosa <divenosavito00@gmail.com>
2023-09-06 14:06:24 +0200

    Add filter 3 and 1

--- home_page/views.py
+++ home_page/views.py
@@ -19,13 +19,31 @@ class HomePage(View):
                       "tag": tag
                       })
     
+    def check_filter_price(self, request, query, price_filter):
+       
+        additional_filtered_items = ''
+        if price_filter == '' or price_filter == '-':
+            additional_filtered_items = query.order_by(f'{price_filter}PriceItem')
+        else:
+            additional_filtered_items = query
+    
+        return additional_filtered_items
 
-    def check_filter(self, request, check_filter):
+    def check_filter(self, request, *args):
+        list_filter_to_applied = {}
+        list_to_check = ['Category', 'Brand', 'Specific Category']
+
+        for x in range(0, len(args)-2):
+            if args[x] != list_to_check[x]:
+                list_filter_to_applied[list_to_check[x]] = args[x]
+            else:
+                pass
+    
+        if len(list_filter_to_applied) > 0:
+            return list_filter_to_applied
+        else:          
+            return list_filter_to_applied
         
-        print(check_filter)
-        
-
-
 
     def post(self, request):
         
@@ -42,38 +60,89 @@ class HomePage(View):
         category = Category.objects.all()
         tag = ShopItem.objects.values_list('TagItem', flat=True).distinct()
 
-        check_filter = {
-            "category_filter" : {
-                "category" : category_choice,
-                "brand" : brand_filter,
-                "sotto_category": specific_category_filter,
-            },
-            "price": price_filter,
-            "new": last_updated_filter,
-            "other" :{
-                "brand" : brand,
-                "category": category,
-                "tag": tag
-            }
-        }
-
+        #Check all items filter
         if all_item:
             items = ShopItem.objects.all()
+
+            additional_filtered_items = self.check_filter_price(request, items, price_filter)
+        
             return render(request, "structure/homepage.html",{
-                      'items': items,
+                      'items': additional_filtered_items,
                       "category" : category,
                       "brand": brand,
                       "tag": tag
                       })
-
-        else:
-            self.check_filter(request, check_filter)
         
-            items = ShopItem.objects.filter(TagItem = "abcdiefghilmnopqrstvuz")
-            return render(request, "structure/homepage.html",{
-                        'items': items,
-                        "category" : category,
+
+        #Check with 3 category filter
+        else:
+            filter = self.check_filter(request, category_choice, brand_filter, specific_category_filter, price_filter, last_updated_filter)
+
+            if len(filter) == 3:
+                
+                items = ShopItem.objects.filter(category__category=category_choice, brand__brand = brand_filter, TagItem = specific_category_filter)
+
+                if items.exists():
+
+                    items = self.check_filter_price(request, items, price_filter)
+
+                    return render(request, "structure/homepage.html",{
+                        "items": items,
+                        "category": category,
                         "brand": brand,
-                        "tag": tag,
-                        "message": "There is no result."
+                        "tag": tag
+                    })
+
+                else:
+                    items = ShopItem.objects.filter(TagItem = "abcdiefghilmnopqrstvuz")
+                    return render(request, "structure/homepage.html",{
+                                'items': items,
+                                "category" : category,
+                                "brand": brand,
+                                "tag": tag,
+                                "message": "There is no result."
+                                })
+                
+
+
+            #Check with 2 category filter  
+
+
+
+
+
+
+
+            #Check with 1 category filter 
+            elif len(filter) == 1:
+                query = Q()
+
+                if category_choice != 'Category':
+                    query &= Q(category__category=category_choice)
+
+                if brand_filter != 'Brand':
+                    query &= Q(brand__brand=brand_filter)
+
+                if specific_category_filter != 'Specific Category':
+                    query &= Q(TagItem=specific_category_filter)
+
+                filtered_items = ShopItem.objects.filter(query)
+
+                if filtered_items.exists():
+                    additional_filtered_items = self.check_filter_price(request, filtered_items, price_filter)
+
+                    return render(request, "structure/homepage.html",{
+                            "items": additional_filtered_items,
+                            "category": category,
+                            "brand": brand,
+                            "tag": tag
                         })
+            else:
+                items = ShopItem.objects.filter(TagItem = "abcdiefghilmnopqrstvuz")
+                return render(request, "structure/homepage.html",{
+                            'items': items,
+                            "category" : category,
+                            "brand": brand,
+                            "tag": tag,
+                            "message": "There is no result."
+                            })
